586fb34ab0b3e505bfa2d094737105fcfebaaa47
kuzminal <kuzmin.al35@gmail.com>
2021-01-25 17:02:46 +0300

    Initial commit

---
+++ src/main/java/com/kuzmin/security/oauth2/github/GithubApplication.java
@@ -0,0 +1,13 @@
+package com.kuzmin.security.oauth2.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kuzmin/security/oauth2/github/GithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuzmin.security.oauth2.github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
